397c23b350d7ffe4f094558ab6f575c27257b96d
Kamil Filip <kamil.filip94@gmail.com>
2020-12-17 18:39:51 +0000

    Clear config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,3 @@
-const { alias } = require("react-app-rewire-alias");
-
 module.exports = function override(config) {
-  return alias({
-    // components: "./src/components",
-    "@components/*": ["sandbox/src/components/*"]
-  })(config);
+  return config;
 };
-
-// const rewireAliases = require("react-app-rewire-aliases");
-// const { paths } = require("react-app-rewired");
-// const path = require("path");
-
-// /* config-overrides.js */
-// module.exports = function override(config, env) {
-//   config = rewireAliases.aliasesOptions({
-//     "@components": path.resolve(__dirname, `${paths.appSrc}/components/`)
-//   })(config, env);
-//   return config;
-// };
