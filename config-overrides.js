eec525120cacab5a3b6abba912baa5b039dbf019
Kamil Filip <kamil.filip94@gmail.com>
2021-01-07 18:45:26 +0100

    [IMPROVEMENT] Optimize SVG files and decrease files size

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,15 @@
 module.exports = function override(config) {
   return config;
 };
+
+// const rewireAliases = require("react-app-rewire-aliases");
+// const { paths } = require("react-app-rewired");
+// const path = require("path");
+
+// /* config-overrides.js */
+// module.exports = function override(config, env) {
+//   config = rewireAliases.aliasesOptions({
+//     "@components": path.resolve(__dirname, `${paths.appSrc}/components/`)
+//   })(config, env);
+//   return config;
+// };
